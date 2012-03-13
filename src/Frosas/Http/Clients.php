1e991e3409f68bdb5115d08bcacef4d08b84e0f5
Francesc Rosàs <francescrosasbosque@gmail.com>
2012-03-13 13:53:03 +0100

    Limit active connections

--- src/Frosas/Http/Clients.php
+++ src/Frosas/Http/Clients.php
@@ -2,52 +2,42 @@
 
 namespace Frosas\Http;
 
+use Zend\Http\Client;
+
 class Clients {
-    
-    private $connectionsPending = array();
+
+    private $connectionsQueued = array();
+    private $connectionsActive = array();
     private $connectionsDone = array();
+    private $maxActiveConnections = 10;
 
     function __construct($clients = null) {
         foreach ((array) $clients as $client) $this->add($client);
     }
 
     function add($client) {
-        if (! $client instanceof \Zend\Http\Client) {
-            $client = new \Zend\Http\Client($client);
-        }
-
-        $client->setAdapter(new Adapter\Writer);
-        $client->send();
-
-        $socket = $client->getAdapter()->socket();
-        stream_set_blocking($socket, 0);
-
-        $rawRequest = $client->getAdapter()->rawRequest();
-        $client->setAdapter(new Adapter\Reader($rawRequest));
-
-        $this->connectionsPending[(int) $socket] = array(
-            'client' => $client,
-            'socket' => $socket
-        );
-
+        if (! $client instanceof Client) $client = new Client($client);
+        $this->connectionsQueued[] = array('client' => $client);
+        $this->fillActiveConnectionsQueue();
         return $client;
     }
 
     function next() {
-        while ($this->connectionsPending && ! $this->connectionsDone) {
+        while ($this->connectionsActive && ! $this->connectionsDone) {
             $sockets = $this->sockets();
             $null = null;
             // TODO Handle timeouts
             stream_select($sockets, $null, $null, 9999);
             foreach ($sockets as $socket) {
-                $connection = $this->connectionsPending[(int) $socket];
+                $connection = $this->connectionsActive[(int) $socket];
                 $client = $connection['client'];
                 $data = stream_get_contents($socket);
                 if ($data === false) throw new \Exception;
                 $client->getAdapter()->appendToResponse($data);
                 if ($data === '') { // End of response
-                    unset($this->connectionsPending[(int) $socket]);
+                    unset($this->connectionsActive[(int) $socket]);
                     $this->connectionsDone[] = $connection;
+                    $this->fillActiveConnectionsQueue(); // Add another one
                 }
             }
         }
@@ -58,10 +48,34 @@ class Clients {
             return $client;
         }
     }
+
+    function setMaxActiveConnections($max) {
+        $this->maxActiveConnections = $max;
+    }
     
     private function sockets() {
         return array_map(function($client) {
             return $client['socket'];
-        }, $this->connectionsPending);        
+        }, $this->connectionsActive);        
+    }
+
+    private function fillActiveConnectionsQueue() {
+        while ($this->connectionsQueued && count($this->connectionsActive) < $this->maxActiveConnections) {
+            $connection = array_shift($this->connectionsQueued);
+
+            // Connect + send request
+            $client = $connection['client'];
+            $client->setAdapter(new Adapter\Writer);
+            $client->send();
+
+            // Prepare client for receiving response
+            $socket = $client->getAdapter()->socket();
+            $connection['socket'] = $socket;
+            stream_set_blocking($socket, 0);
+            $rawRequest = $client->getAdapter()->rawRequest();
+            $client->setAdapter(new Adapter\Reader($rawRequest));
+
+            $this->connectionsActive[(int) $socket] = $connection;
+        }
     }
 }
