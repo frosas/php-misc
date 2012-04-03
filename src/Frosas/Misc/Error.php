1330c30c74bfe34107e5376598272e38f867ecc2
Francesc Rosàs <francescrosasbosque@gmail.com>
2012-04-03 12:47:17 +0200

    PHP already has an ErrorException!

---
+++ src/Frosas/Misc/Error.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Frosas\Misc;
+
+/**
+ * Utilities around PHP errors
+ */
+class Error {
+    
+    static function createExceptionFromLast() {
+        $error = error_get_last();
+        if (! $error) throw new \RuntimeException("Hasn't been any error yet");
+        return new \ErrorException($error['message'], 0, $error['type'], $error['file'], $error['line']);
+    }
+}
