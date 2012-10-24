3e64ce678d43ec82dd074a5a4c435fd56703cf00
Francesc Rosàs <francescrosasbosque@gmail.com>
2012-10-24 18:07:04 +0200

    Json::encode()

--- src/Frosas/Misc/Json.php
+++ src/Frosas/Misc/Json.php
@@ -4,10 +4,34 @@ namespace Frosas\Misc;
 
 class Json
 {
+    /**
+     * @see json_encode()
+     *
+     * Some errors (like encoding recursive arrays or resources) are not caught.
+     * Having an error handler that converts them to exceptions is highly
+     * recommended.
+     */
+    static function encode()
+    {
+        $json = call_user_func_array('json_encode', func_get_args());
+        self::throwLastError();
+        return $json;
+    }
+
+    /**
+     * @see json_decode()
+     */
     static function decode()
     {
         $data = call_user_func_array('json_decode', func_get_args());
-        if (json_last_error() !== JSON_ERROR_NONE) throw new \InvalidArgumentException;
+        self::throwLastError();
         return $data;
+    }
+
+    private static function throwLastError()
+    {
+        $lastError = json_last_error();
+        if ($lastError === JSON_ERROR_NONE) return;
+        throw new \InvalidArgumentException("Error #$lastError");
     }
 }
--- tests/Frosas/Misc/JsonTest.php
+++ tests/Frosas/Misc/JsonTest.php
@@ -4,6 +4,32 @@ namespace Frosas\Misc;
 
 class JsonTest extends \PHPUnit_Framework_TestCase
 {
+    function testEncodeValidData()
+    {
+        $original = array(1, 2, 3);
+        $json = Json::encode($original);
+        $this->assertEquals($original, json_decode($json));
+    }
+
+    function testEncodeThrowsInvalidArgumentException()
+    {
+        $this->markTestIncomplete("How to test it?");
+    }
+
+    function testFailEncodingRecursiveArray()
+    {
+        $this->setExpectedException('PHPUnit_Framework_Error_Warning');
+        $array = array();
+        $array[] =& $array;
+        Json::encode($array);
+    }
+
+    function testFailEncodingResource()
+    {
+        $this->setExpectedException('PHPUnit_Framework_Error_Warning');
+        Json::encode(tmpfile());
+    }
+
     function testDecodeValidJson()
     {
         $original = array(1, 2, 3);
