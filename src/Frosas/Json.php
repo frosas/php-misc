9272317c8db1ae666647d3d55286a2df2eaec399
Francesc Rosàs <francescrosasbosque@gmail.com>
2012-11-09 00:10:59 +0100

    Better error messages

--- src/Frosas/Json.php
+++ src/Frosas/Json.php
@@ -30,8 +30,20 @@ class Json
 
     private static function throwLastError()
     {
-        $lastError = json_last_error();
-        if ($lastError === JSON_ERROR_NONE) return;
-        throw new \InvalidArgumentException("Error #$lastError");
+        $error = json_last_error();
+        if ($error === JSON_ERROR_NONE) return;
+        throw new \InvalidArgumentException(self::getErrorMessage($error));
+    }
+
+    private static function getErrorMessage($error)
+    {
+        switch ($error) {
+            case JSON_ERROR_DEPTH: return "Maximum stack depth exceeded";
+            case JSON_ERROR_STATE_MISMATCH: return "Underflow or the modes mismatch";
+            case JSON_ERROR_CTRL_CHAR: return "Unexpected control character found";
+            case JSON_ERROR_SYNTAX: return "Syntax error, malformed JSON";
+            case JSON_ERROR_UTF8: return "Malformed UTF-8 characters, possibly incorrectly encoded";
+            default: return "Unknown error #$error";
+        }
     }
 }
