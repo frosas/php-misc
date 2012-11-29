7c31fe19e6e83e6473f353cd231650996cb3abc5
Francesc Rosàs <francescrosasbosque@gmail.com>
2012-11-29 23:41:21 +0100

    Default message

--- src/Frosas/NotFoundException.php
+++ src/Frosas/NotFoundException.php
@@ -2,6 +2,13 @@
 
 namespace Frosas;
 
-class NotFoundException extends \Exception 
+use Exception;
+
+class NotFoundException extends \Exception
 {
+    function __construct($message = "", $code = 0, Exception $previous = null)
+    {
+        if (! $message) $message = "Not found";
+        parent::__construct($message, $code, $previous);
+    }
 }
